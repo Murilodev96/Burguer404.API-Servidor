cc412a74e72e717295dd5161db860cb7a276b26b
Moises Loesch <moisesloesch.loesch1@gmail.com>
2025-04-15 00:52:41 -0300

    Adicionando css no datatable de pedidos

--- Burguer404.Frontend/wwwroot/js/pedido.js
+++ Burguer404.Frontend/wwwroot/js/pedido.js
@@ -1,4 +1,7 @@
 ﻿$(document).ready(function () {
+
+
+
     $('#tabelaPedidos').DataTable({
         ajax: {
             type: "GET",
@@ -19,7 +22,7 @@
         ],
         language: {
             url: "//cdn.datatables.net/plug-ins/1.13.6/i18n/pt-BR.json"
-        }
+        }    
     });
 });
 
