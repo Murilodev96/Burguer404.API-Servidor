fd875eae9508cc6bb7ed78d7327a713b60e9d61e
Murilodev96 <86988927+Murilodev96@users.noreply.github.com>
2025-09-15 22:53:53 -0300

    Ajuste tipo VM

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_B1s"
+    vm_size    = "Standard_D4s_v3"
   }
 
   identity {
